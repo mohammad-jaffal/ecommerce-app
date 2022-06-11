ce3ddc419f2cc3caf87dced7ddb154a22b0d8d02
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-06-11 18:13:32 +0300

    fetching categories

--- electron-admin/JS/admin.js
+++ electron-admin/JS/admin.js
@@ -1,10 +1,21 @@
-window.onload = function () {
+window.onload = async function () {
 
     var token = localStorage.getItem('token')
     // alert(token)
+    var categories = document.getElementById('categories')
     
 
 
+// getting all categories
+    await axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8000/api/user/getcategories',
+    }).then(function (response) {
+        var cats = response.data;
+        for (var i = 0; i < cats['categories'].length; i++) {
+            categories.innerHTML += `<option value="${cats['categories'][i]['id']}">${cats['categories'][i]['name']}</option>`
+        }
+    })
 
 
 
